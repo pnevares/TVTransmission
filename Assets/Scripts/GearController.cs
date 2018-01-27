5f541c435b6b246a6a60fc4fe20fbeedc171eca4
Pablo Nevares <pablo@nevares.com>
2018-01-27 13:14:06 -0700

    Added counter-clockwise animation for gears

---
+++ Assets/Scripts/GearController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearController : MonoBehaviour {
+    public bool reverse;
+
+	// Use this for initialization
+	void Start () {
+        Animator anim = gameObject.GetComponent<Animator>();
+        if(reverse) {
+            anim.SetBool("reverse", true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
