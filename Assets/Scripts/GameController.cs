b68342ab6ebe11ba9264d9ebe94f5efae6e954dd
Pablo Nevares <pablo@nevares.com>
2018-01-27 11:53:24 -0700

    Added post prefab and script for which post is selected

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject[] posts;
+    public int[] correctGears;
+
+    private int selectedPost = 0;
+
+	void Start () {
+        SelectPost(0);
+	}
+	
+	void Update () {
+		
+	}
+
+    public void SelectPost(int postId) {
+        Animator anim = posts[postId].GetComponent<Animator>();
+        anim.enabled = true;
+    }
+}
